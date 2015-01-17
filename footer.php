e8384e7a29167ccc30f258bec3cfe91651af4cf8
seafoam6 <steve@stevebarman.com>
2015-01-17 16:57:02 -0500

    adds home page template

--- footer.php
+++ footer.php
@@ -11,11 +11,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'parkview' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'parkview' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'parkview' ), 'parkview', '<a href="http://stevebarman.com" rel="designer">Steve Barman</a>' ); ?>
-		</div><!-- .site-info -->
+		<img class="foot-florish" src="florish.jpg">
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
---
+++ front-page.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the Home page.
+ *
+ *
+ * @package parkview
+ */
+
+get_header(); ?>
+	<div class="hero">
+	  <div class="issue-number"></div>
+	  <div class="availablity">Available Now</div>
+	  <h1 class="overlay-title">Parkview</h1>
+	</div>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<section>
+		    <h2 class="section-head">About</h2>
+		    <img class="leaf-flourish" src="leaf.png">
+		    <p>PARKVIEW MAGAZINE is a collection of life’s allure through a showcase of design, entertaining, awesome and inspiring people, shared experiences, and spaces. We capture how individual taste is cultivated through living and common spaces and encourage sharing life’s profound moments in them. Not so much of a keeping up with the trends type of read, but more of a cultured visual and reading experience that embraces and influences a lifestyle, PARKVIEW MAGAZINE is a reference for inspiration and cunning perspective on external environments. Our dialogue with some of the most intriguing human beings around, in some of the most aesthetically and functionally pleasing areas we can find, PARKVIEW MAGAZINE is captivating in its own right, submerging the reader into the glory of each page.</p>
+		  </section>
+		  <section>
+		    <h2 class="section-head">Issues</h2>
+		    <div class="issue">
+		      <img src="cover1.jpg">
+		      <div class="issue-desc">
+		        <span class="date">January 2015</span>
+		        <span class="buyit">Buy It Here</span>
+		      </div>
+		    </div>
+		  </section>
+		  <section>
+		    <h2 class="section-head">Contact</h2>
+		    <div class="contact-info">Ashley K. Parks<br><a href="mailto:ashkparks@gmail.com">ashkparks@gmail.com</a>
+		  </section>
+		  <section>
+		    <h2 class="section-head">Connect with us</h2>
+		    <ul class="social-share"></ul>
+		  </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
