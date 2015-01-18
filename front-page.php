5d26a8cf6dcac2e8bc8a2f7b3c0f66142e2045e4
seafoam6 <steve@stevebarman.com>
2015-01-18 14:32:11 -0500

    Home page styling and hero section

--- front-page.php
+++ front-page.php
@@ -10,14 +10,13 @@ get_header(); ?>
 	<div class="hero">
 	  <div class="issue-number"></div>
 	  <div class="availablity">Available Now</div>
-	  <h1 class="overlay-title">Parkview</h1>
 	</div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<section>
 		    <h2 class="section-head">About</h2>
 		    <img class="leaf-flourish" src="leaf.png">
-		    <p>PARKVIEW MAGAZINE is a collection of life’s allure through a showcase of design, entertaining, awesome and inspiring people, shared experiences, and spaces. We capture how individual taste is cultivated through living and common spaces and encourage sharing life’s profound moments in them. Not so much of a keeping up with the trends type of read, but more of a cultured visual and reading experience that embraces and influences a lifestyle, PARKVIEW MAGAZINE is a reference for inspiration and cunning perspective on external environments. Our dialogue with some of the most intriguing human beings around, in some of the most aesthetically and functionally pleasing areas we can find, PARKVIEW MAGAZINE is captivating in its own right, submerging the reader into the glory of each page.</p>
+		    <p><span class="namesake">PARKVIEW MAGAZINE</span> is a collection of life’s allure through a showcase of design, entertaining, awesome and inspiring people, shared experiences, and spaces. We capture how individual taste is cultivated through living and common spaces and encourage sharing life’s profound moments in them. Not so much of a keeping up with the trends type of read, but more of a cultured visual and reading experience that embraces and influences a lifestyle, <span class="namesake">PARKVIEW MAGAZINE</span> is a reference for inspiration and cunning perspective on external environments. Our dialogue with some of the most intriguing human beings around, in some of the most aesthetically and functionally pleasing areas we can find, <span class="namesake">PARKVIEW MAGAZINE</span> is captivating in its own right, submerging the reader into the glory of each page.</p>
 		  </section>
 		  <section>
 		    <h2 class="section-head">Issues</h2>
