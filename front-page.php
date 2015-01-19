9dd55f44b56a1d0770aea67a4fe7ca073a19be2d
seafoam6 <steve@stevebarman.com>
2015-01-19 07:41:22 -0500

    typography

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@ get_header(); ?>
 	</div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<section>
+			<section id="about" class="about">
 		    <h2 class="section-head">About</h2>
 		    <img class="leaf-flourish" src="leaf.png">
 		    <p><span class="namesake">PARKVIEW MAGAZINE</span> is a collection of life’s allure through a showcase of design, entertaining, awesome and inspiring people, shared experiences, and spaces. We capture how individual taste is cultivated through living and common spaces and encourage sharing life’s profound moments in them. Not so much of a keeping up with the trends type of read, but more of a cultured visual and reading experience that embraces and influences a lifestyle, <span class="namesake">PARKVIEW MAGAZINE</span> is a reference for inspiration and cunning perspective on external environments. Our dialogue with some of the most intriguing human beings around, in some of the most aesthetically and functionally pleasing areas we can find, <span class="namesake">PARKVIEW MAGAZINE</span> is captivating in its own right, submerging the reader into the glory of each page.</p>
