a38e76bd8a1b6c41caff59cd8336309c99c27190
seafoam6 <steve@stevebarman.com>
2015-01-21 07:46:37 -0500

    custom slider work

--- dist/all.js
+++ dist/all.js
@@ -6075,8 +6075,19 @@
 } )();
 
 jQuery(document).ready(function($) {
-    
+    var slides = $('.hero .slide');
+    var totalSlides = slides.length;
+    // for (var i =0; i<= totalSlides; i++){
+    //   $()
+    // }
+    $.each(slides,function(index,el){
+      console.log(index + " "+el);
+    });
+
+    console.log($(slides));
+    $(slides[1]).css('opacity',1);
 });
+
 ( function() {
 	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
 	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
--- js/site.js
+++ js/site.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function($) {
-    
+    var slides = $('.hero .slide');
+    var totalSlides = slides.length;
+    // for (var i =0; i<= totalSlides; i++){
+    //   $()
+    // }
+    $.each(slides,function(index,el){
+      console.log(index + " "+el);
+    });
+
+    console.log($(slides));
+    $(slides[1]).css('opacity',1);
 });
