ada84200347042bbc60880019f8eb21e1927aec1
seafoam6 <steve@stevebarman.com>
2015-01-20 20:26:47 -0500

    enques jquery

--- functions.php
+++ functions.php
@@ -106,11 +106,21 @@ function parkview_scripts() {
 
 	wp_enqueue_script( 'parkview-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script(
+		'site-js',
+		get_stylesheet_directory_uri() . '/js/site.js',
+		array( 'jquery' )
+	);
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'parkview_scripts' );
+
+	
+
+
 
 /**
  * Implement the Custom Header feature.
