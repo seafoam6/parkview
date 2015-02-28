01f5abaedde193403f6c845511d53664295ded23
seafoam6 <steve@stevebarman.com>
2015-02-28 10:41:29 -0500

    cleans out unused files

--- functions.php
+++ functions.php
@@ -170,22 +170,4 @@ function parkview_theme_options() {
  *
  */
 
-if ( !function_exists( 'of_get_option' ) ) {
-function of_get_option($name, $default = false) {
 
-	$optionsframework_settings = get_option('optionsframework');
-
-	// Gets the unique option id
-	$option_name = $optionsframework_settings['id'];
-
-	if ( get_option($option_name) ) {
-		$options = get_option($option_name);
-	}
-
-	if ( isset($options[$name]) ) {
-		return $options[$name];
-	} else {
-		return $default;
-	}
-}
-}
